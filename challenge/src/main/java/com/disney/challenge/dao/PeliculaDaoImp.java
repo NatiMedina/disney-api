1276854d17d9ec3301fca0dc74c1106434b44018
NatiMedina <natymedina0589@gmail.com>
2022-03-09 15:43:39 -0300

    refactor Pelicula

---
+++ challenge/src/main/java/com/disney/challenge/dao/PeliculaDaoImp.java
@@ -0,0 +1,2 @@
+package com.disney.challenge.dao;public class PeliculaDaoImp {
+}
--- challenge/src/main/java/com/disney/challenge/models/Pelicula.java
+++ challenge/src/main/java/com/disney/challenge/models/Pelicula.java
@@ -5,7 +5,10 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "peliculas")
 @ToString
 @Getter
 @Setter
