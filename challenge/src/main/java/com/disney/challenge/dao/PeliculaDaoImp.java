f6a44a77a48dd6dfdbad8a75eeb18243a519e20d
NatiMedina <natymedina0589@gmail.com>
2022-03-10 14:51:20 -0300

    create implementation of PeliculaDao

--- challenge/src/main/java/com/disney/challenge/dao/PeliculaDaoImp.java
+++ challenge/src/main/java/com/disney/challenge/dao/PeliculaDaoImp.java
@@ -1,2 +1,62 @@
-package com.disney.challenge.dao;public class PeliculaDaoImp {
+package com.disney.challenge.dao;
+
+import com.disney.challenge.models.Pelicula;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Date;
+import java.util.List;
+
+@Repository
+@Transactional
+
+
+public class PeliculaDaoImp implements PeliculaDao{
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public List<Pelicula> getPeliculas() {
+        String query = "FROM Pelicula";
+        return entityManager.createQuery(query).getResultList();
+    }
+
+    @Override
+    public Pelicula crearPelicula(Pelicula pelicula) {
+        entityManager.persist(pelicula);
+        return pelicula;
+    }
+
+    @Override
+    public void eliminarPelicula(Long id) {
+        Pelicula pelicula = entityManager.find(Pelicula.class, id);
+        if(pelicula != null){
+            entityManager.remove(pelicula);
+        }
+    }
+
+    @Override
+    public Pelicula modificarPelicula(Long id, Pelicula pelicula) {
+        Pelicula oldPelicula = entityManager.find(Pelicula.class, id);
+        if(oldPelicula != null && pelicula != null){
+
+            if(pelicula.getImagen() != null){
+                oldPelicula.setImagen(pelicula.getImagen());
+            }
+            if(pelicula.getTitulo() != null){
+                oldPelicula.setTitulo(pelicula.getTitulo());
+            }
+            if(pelicula.getCreacion() != null){
+                oldPelicula.setCreacion(pelicula.getCreacion());
+            }
+            oldPelicula.setCalificacion(pelicula.getCalificacion());
+            if(pelicula.getPersonajes() != null){
+                oldPelicula.setPersonajes(pelicula.getPersonajes());
+            }
+        }
+        return oldPelicula;
+    }
 }
