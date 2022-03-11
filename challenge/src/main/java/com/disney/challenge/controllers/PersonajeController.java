4de9e19845120f691578840b27e0e42bf4db36d2
NatiMedina <natymedina0589@gmail.com>
2022-03-11 18:38:14 -0300

    create personaje-controller

--- challenge/src/main/java/com/disney/challenge/controllers/PersonajeController.java
+++ challenge/src/main/java/com/disney/challenge/controllers/PersonajeController.java
@@ -1,4 +1,27 @@
 package com.disney.challenge.controllers;
 
+import com.disney.challenge.dao.PersonajeDao;
+import com.disney.challenge.models.Personaje;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class PersonajeController {
+
+    @Autowired
+    private PersonajeDao personajeDao;
+
+    @RequestMapping(value = "/character", method = RequestMethod.POST)
+    public Personaje createPersonaje(@RequestBody Personaje personaje){
+        return personajeDao.crearPersonaje(personaje);
+    }
+    @RequestMapping(value = "/characters", method = RequestMethod.GET)
+    public List<List<String>> getPersonajes(){
+        return personajeDao.getPersonajes();
+    }
 }
