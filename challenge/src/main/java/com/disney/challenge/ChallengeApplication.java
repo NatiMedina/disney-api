7d58c06a27d926e9a5519ca6458d28724f0106b0
NatiMedina <natymedina0589@gmail.com>
2022-03-14 11:28:46 -0300

    change database

--- challenge/src/main/java/com/disney/challenge/ChallengeApplication.java
+++ challenge/src/main/java/com/disney/challenge/ChallengeApplication.java
@@ -2,8 +2,10 @@ package com.disney.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class ChallengeApplication {
 
 	public static void main(String[] args) {
